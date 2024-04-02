27c12f388f45466b4103f98563c8a202a61e77d8
Nikolay Osadchyi <148019339+Kelmarji@users.noreply.github.com>
2024-04-02 10:37:13 +0300

    RmT

--- src/components/LeftFilter/LeftFilter.js
+++ src/components/LeftFilter/LeftFilter.js
@@ -7,11 +7,26 @@ const LeftFilter = () => {
   return (
     <div className={f.filterList}>
       <h3>количество пересадок</h3>
-      <a.Checkbox>Все</a.Checkbox>
-      <a.Checkbox>1 пересадка</a.Checkbox>
-      <a.Checkbox>2 пересадки</a.Checkbox>
-      <a.Checkbox>3 пересадки</a.Checkbox>
-      <a.Checkbox>без Пересадок</a.Checkbox>
+      <a.ConfigProvider
+        theme={{
+          components: {
+            Checkbox: {
+              colorWhite: '#2196F3', // галочка
+              colorBorder: '#9ABBCE', // color border
+              colorPrimaryBorder: '#9ABBCE',
+              colorPrimaryHover: 'white',
+              // colorPrimary: 'white',
+              /* here is your component tokens */
+            },
+          },
+        }}
+      >
+        <a.Checkbox>Все</a.Checkbox>
+        <a.Checkbox>без Пересадок</a.Checkbox>
+        <a.Checkbox>1 пересадка</a.Checkbox>
+        <a.Checkbox>2 пересадки</a.Checkbox>
+        <a.Checkbox>3 пересадки</a.Checkbox>
+      </a.ConfigProvider>
     </div>
   );
 };
